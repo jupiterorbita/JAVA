c999bfdcd3027df8ca3d7dea892e5e09d3f66566
jupterorbita <john.williams@outlook.com>
2018-06-02 22:29:47 -0700

    first Java program!

---
+++ javaFun/FirstJavaProgramm.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgramm{
+    public static void main(String[] args){
+        System.out.println("My name is Coding Dojo");
+        System.out.println("I am 9999 years old");
+        System.out.println("I live here =) ");
+    }
+}
